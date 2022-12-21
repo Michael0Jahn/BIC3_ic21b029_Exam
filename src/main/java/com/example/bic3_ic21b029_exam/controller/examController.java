9fb0ef14dc645cf475ed0f90e48fb4df91a5315e
Michael0Jahn <ic21b029@technikum-wien.at>
2022-12-21 18:34:39 +0100

    added new feature  gradeInPercentage

--- src/main/java/com/example/bic3_ic21b029_exam/controller/examController.java
+++ src/main/java/com/example/bic3_ic21b029_exam/controller/examController.java
@@ -8,4 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class examController {
 
+    @GetMapping("/api/mark")
+
+    public int gradeInPercentage (int percentage) {
+        percentage = 0;
+
+        int gradeOne = 1; int gradeTwo = 2; int gradeThree = 3; int gradeFour = 4; int gradeFive = 5;
+
+        if (percentage < 50) {
+            return gradeFive;
+        } else if (percentage >= 50 && percentage < 63) {
+            return gradeFour;
+        } else if (percentage >= 63 && percentage < 75) {
+            return gradeThree;
+        } else if (percentage >= 75 && percentage < 88) {
+            return gradeTwo;
+        } else if (percentage >= 88) {
+            return gradeOne;
+        }
+        else return 0;
+    }
 }

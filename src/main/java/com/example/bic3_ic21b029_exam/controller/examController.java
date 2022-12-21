db9aff7db4459b848b62987932e70f26d1bef587
Michael0Jahn <ic21b029@technikum-wien.at>
2022-12-21 18:14:58 +0100

    added Restcontroller to examController

--- src/main/java/com/example/bic3_ic21b029_exam/controller/examController.java
+++ src/main/java/com/example/bic3_ic21b029_exam/controller/examController.java
@@ -1,4 +1,11 @@
 package com.example.bic3_ic21b029_exam.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
 public class examController {
+
 }

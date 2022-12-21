abf9b83d4980070b6214ae1e99219eb994030e78
Michael0Jahn <ic21b029@technikum-wien.at>
2022-12-21 18:01:10 +0100

    Initial commit

---
+++ src/main/java/com/example/bic3_ic21b029_exam/Bic3Ic21b029ExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bic3_ic21b029_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bic3Ic21b029ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bic3Ic21b029ExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bic3_ic21b029_exam/Bic3Ic21b029ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bic3_ic21b029_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bic3Ic21b029ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
